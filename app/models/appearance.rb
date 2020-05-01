81d5f045a724a4ad3d79933d623fbf9172f1da43
github username <github email address>
2020-05-01 11:52:49 -0400

    final commit

--- app/models/appearance.rb
+++ app/models/appearance.rb
@@ -7,4 +7,6 @@ class Appearance < ApplicationRecord
 
   validates :rating, numericality: {greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
 
+  #not 100 percent sure about numericality syntax
+
 end
