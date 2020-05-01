4c161546e2660c22f851d1cc8d684e596ce1e07f
dkersten <dkersten1994@gmail.com>
2020-05-01 11:50:43 -0400

    deliverables

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -2,13 +2,15 @@ class Episode < ApplicationRecord
     has_many :appearances
     has_many :guests, through: :appearances
 
-    def avg_rating
-        rating_array = []
-        self.appearances.each do |appearance|
-            rating_array << appearance.rating.to_i
-        end
+    # def avg_rating
+    #     rating_array = []
+    #     self.appearances.each do |appearance|
+    #         rating_array << appearance.rating.to_i
+    #     end
 
-        avg = (rating_array.sum / rating_array.length )
-        avg
-    end
+    #     if rating_array != 0 
+    #         avg = (rating_array.sum / rating_array.length )
+    #     end
+    #     avg
+    # end
 end
