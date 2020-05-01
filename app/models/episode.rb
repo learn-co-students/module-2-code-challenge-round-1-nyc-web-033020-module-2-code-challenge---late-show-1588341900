fd083cc3f8c70d4c6fe526d3bcb93c951f98a209
dkersten <dkersten1994@gmail.com>
2020-05-01 11:34:11 -0400

    added 2nd advanced requirement

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,4 +1,14 @@
 class Episode < ApplicationRecord
     has_many :appearances
     has_many :guests, through: :appearances
+
+    def avg_rating
+        rating_array = []
+        self.appearances.each do |appearance|
+            rating_array << appearance.rating.to_i
+        end
+
+        avg = (rating_array.sum / rating_array.length )
+        avg
+    end
 end
